d184712b93686f8708c94f853fe8344e77b62be3
pkali076 <77549872+pkali076@users.noreply.github.com>
2024-06-29 16:13:35 -0400

    stock-trading updates with Python main app script

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+API_KEY = 'AlphaVintageAPIKey'
+BASE_URL = 'https://www.alphavintage.co/query'
+
+def get_stock_data(symbol, interval='1min', outputsize='compact'):
+    param = {
+        'function': 'TIME_SERIES_INTRADAY',
+        'symbol': symbol,
+        'interval': interval,
+        'apikey': API_KEY,
+        'outputsize': outputsize
+    }
+
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+    time_series_key = f'Time Series ({interval})'
+    if time_series_key in data:
+        return data[time_series_key]
+    else:
+        return {'error': 'Error fetching data'}
+    
+@app.route('/', methods=['POST'])
+def index():
+    data = request.get_json()
+    symbol = data['symbol'].upper()
+    stock_data = get_stock_data(symbol)
+    return jsonify(stock_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
