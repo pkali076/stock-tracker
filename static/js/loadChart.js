988afbf3de0222b10ed5142423598309b640d481
pkali076 <77549872+pkali076@users.noreply.github.com>
2024-06-30 09:58:29 -0400

    testing MyLibrary import for loadChart.js

--- static/js/loadChart.js
+++ static/js/loadChart.js
@@ -1,4 +1,4 @@
-import webpackConfig from "../../webpack.config";
+import MyLibrary from "../../webpack.config";
 //import MyLibrary from webpackConfig
 
 document.addEventListener('DOMContentLoaded', (event) => {
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const data = JSON.parse(document.getElementById('chartData').textContent);
 
     // Make sure MyLibrary is accessible
-    if (typeof webpackConfig.output.library.name !== 'undefined' && webpackConfig.output.library.name.createChart) {
-        webpackConfig.output.library.name.createChart(labels, data);
+    if (typeof MyLibrary !== 'undefined' && MyLibrary.createChart) {
+        MyLibrary.createChart(labels, data);
     } else {
         console.error('MyLibrary or createChart is not defined');
     }
